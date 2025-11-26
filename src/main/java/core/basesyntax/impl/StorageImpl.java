9d721bc5c30c973388f7c65a28f6def116423a5f
Andrii <andreymaluk35@gmail.com>
2025-11-27 00:27:33 +0200

    Fix #2

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -6,22 +6,33 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     private K[] keys;
     private V[] values;
     private int size;
+    private static final int CAPACITY = 10;
 
-    @SuppressWarnings("unchecked")
+
     public StorageImpl() {
-        this.keys = (K[]) new Object[10];
-        this.values = (V[]) new Object[10];
-
+        this.keys = new K[CAPACITY];
+        this.values = new V[CAPACITY];
+        this.size = 0;
     }
+
+    private int getKeyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key != null ? key.equals(keys[i]) : keys[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size; i++) {
-            if (key != null ? key.equals(keys[i]) : keys[i] == null) {
-                values[i] = value;
-                return;
-            }
+        int index = getKeyIndex(key);
+        if (index != -1) {
+            values[index] = value;
+            return;
         }
+
         keys[size] = key;
         values[size] = value;
         size++;
@@ -29,12 +40,8 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if (key != null ? key.equals(keys[i]) : keys[i] == null) {
-                return values[i];
-            }
-        }
-        return null;
+        int index = getKeyIndex(key);
+        return index != -1 ? values[index] : null;
     }
 
     @Override
